bebe886372f1522ee072ad7b5ed3e8ce23d9a8cf
VenkataRamachandrudu <chanduchamakuri@gmail.com>
2025-09-10 11:42:39 +0530

    commit

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ if (analyzeButton) {
 
       // Send to webhook
       const response = await fetch(
-        'https://ajaychanumolu05.app.n8n.cloud/webhook/meal_ai',
+        'https://venkataramachandruduch.app.n8n.cloud/webhook-test/meal_ai',
         {
           method: 'POST',
           body: formData,
